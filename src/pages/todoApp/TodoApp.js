f27d5fbf3e4ecdc81093d9a801bdd1247a95192e
Norayr <norayr.abgaryan@gmail.com>
2022-08-14 18:11:20 +0400

    created listItem

--- src/pages/todoApp/TodoApp.js
+++ src/pages/todoApp/TodoApp.js
@@ -1,19 +1,25 @@
 import { useState } from "react";
 import AddTodoItem from "./components/addTodoItem/AddTodoItem";
+import List from "./components/list/List";
+import ListItem from "./components/listItem/ListItem";
 
 
 const TodoApp = () => {
-const [listItems, setListItems] = useState([]);
-const onAdd = (value) => {
-  const arr = [value, ...listItems];
+  const [listItems, setListItems] = useState([]);
+  const onAdd = (value) => {
+    if(value.trim()){
+    const arr = [value, ...listItems];
 
-  setListItems(arr);
-};
+    setListItems(arr);
+  }
+  };
 
-console.log(listItems);
-    return (
-      <AddTodoItem onAdd={onAdd}/>
-    );
+  return (
+    <div>
+      <AddTodoItem onAdd={onAdd} />
+      <List items={listItems}/>
+    </div>
+  );
 };
 
 export default TodoApp;
